277f8ffe7e16f6829b22459fa403cbe0fbd5567b
Brian Liao <yaramiu@gmail.com>
2023-03-30 12:18:55 -0400

    Create contact tab contents

---
+++ src/contact-tab.js
@@ -0,0 +1,33 @@
+import vaneAndPercivalPng from "./images/contact/vane-and-percival.png";
+import siegfriedAndLancealotPng from "./images/contact/siegfried-and-lancealot.png";
+
+export default function contactTab() {
+  const contentDiv = document.querySelector("#content");
+
+  const contactUsHeader = document.createElement("h1");
+  contactUsHeader.textContent = "Contact Us";
+  contentDiv.appendChild(contactUsHeader);
+
+  const contactImagesDiv = document.createElement("div");
+  contactImagesDiv.classList.add("contact-images");
+  const vaneAndPercivalImage = new Image();
+  vaneAndPercivalImage.src = vaneAndPercivalPng;
+  vaneAndPercivalImage.setAttribute(
+    "alt",
+    "A man with yellow hair wearing a beret, white t-shirt and black pants standing next to a man with red hair wearing a greyish-brown dress shirt and white pants"
+  );
+  const siegfriedAndLancealotImage = new Image();
+  siegfriedAndLancealotImage.src = siegfriedAndLancealotPng;
+  siegfriedAndLancealotImage.setAttribute(
+    "alt",
+    "A man with brown hair wearing a blueish chef jacket and black pants standing next to a man with black hair wearing a waiters outfit"
+  );
+  contactImagesDiv.appendChild(vaneAndPercivalImage);
+  contactImagesDiv.appendChild(siegfriedAndLancealotImage);
+  contentDiv.appendChild(contactImagesDiv);
+
+  const contactParagraph = document.createElement("p");
+  contactParagraph.textContent =
+    "Have any questions, comments, or concerns? Contact us by calling 111-111-1111 or shoot us an email at dragonknights@notarealemail.com";
+  contentDiv.appendChild(contactParagraph);
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import initializePageLoad from "./initialize-page-load.js";
 import menuTab from "./menu-tab.js";
+import contactTab from "./contact-tab.js";
 import "./style.css";
 
 //initializePageLoad();
-menuTab();
+//menuTab();
+contactTab();
